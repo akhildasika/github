e590e3074c53ca344d46cda414e2371c8c016e41
Akhil Dasika <akhildasika@codelab-26.rockhurst.int>
2020-02-04 08:36:18 -0600

    Syntax Notes

--- HelloGit.py
+++ HelloGit.py
@@ -4,13 +4,45 @@ a = 1
 b = 5
 print(a + b)
 
+#list
+myList = {1, 2, 3, 4, 3, 2, 1}
 
-myList = {1 ,2 ,3}
+print(myList[0:6:2])
 
-print(myList[1])
+# strings are just lists
+phrase = 'Catch the Dog'
+phrase[2] #t
+phrase[4] #h
+phrase[-1:0:-3] #strides backwards, -1 right, 0 is l$
 
-def sumFunction(a, b): 
+#function
+def sumfunction(a, b): 
 	return a + b
 
-print(sumFunction(2, 20))
+print(sumfunction(2, 20))
 
+#if loop
+if(x == 10):
+	x = 5
+elif(y == -10):
+	y = 5
+else:
+	x = y-x
+
+#loop
+Lst = {1, 3, 8, 412, 43, 2, 20}
+#length of list
+len(Lst)
+
+#for loop by index
+for i in range(len(Lst)):
+	print(Lst[i])
+#for each loop
+for val in Lst:
+	print(val)
+#dictionaries
+lookup = {}
+lookup['kc'] = 'Chiefs'
+lookup['ne'] = 'patriots'
+lookup['la'] = 'chargers'
+lookup['suck'] = 'raiders'
